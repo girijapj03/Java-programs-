312eb6a941f6cc62582c50bdb982da6edfa2d93a
girijapj03 <72901278+girijapj03@users.noreply.github.com>
2020-10-19 17:27:15 +0530

    Add files via upload
    here we created object and class

---
+++ Flower.java
@@ -0,0 +1,7 @@
+class Flower{
+String name;
+String color;
+void blooming() {
+System.out.println(color+ " "+name +" is blooming");
+}
+}
---
+++ FlowerDemo.java
@@ -0,0 +1,13 @@
+class FlowerDemo{
+public static void main(String args[]){
+Flower rose=new Flower();
+rose.name="rose";
+rose.color="red";
+rose.blooming();
+
+Flower jasmin=new Flower();
+jasmin.name="jasmin";
+jasmin.color="white";
+jasmin.blooming();
+}
+}
